86ad1b285fe5bf01d74f3c975c2f3d3b9ba9c584
gnb2000 <barirodriguez@gmail.com>
2023-05-04 20:48:32 -0300

    Add cross origin *

--- src/main/java/com/gnb/weatherapi/controllers/WeatherController.java
+++ src/main/java/com/gnb/weatherapi/controllers/WeatherController.java
@@ -3,10 +3,7 @@ package com.gnb.weatherapi.controllers;
 import com.gnb.weatherapi.dtos.WeatherResponse;
 import com.gnb.weatherapi.services.WeatherService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,6 +13,7 @@ public class WeatherController {
     private final WeatherService weatherService;
 
     @GetMapping("/city")
+    @CrossOrigin(origins = "*")
     public WeatherResponse getWeatherByCity(@RequestParam String city) {
         return weatherService.getWeatherByCity(city);
     }
